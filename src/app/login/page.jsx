77ccdb766d8b31abb534a9d5c9819dede6ee4e20
Sarthak-Srivastava-01 <sarthak.srivastava1398@gmail.com>
2024-09-05 10:58:29 +0530

    react pages setup

---
+++ src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <h1 className='text-3xl text-center mt-5'>Login</h1>
+    </div>
+  )
+}
+
+export default Login;
---
+++ src/app/signup/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+        <h1 className='text-3xl text-center mt-5'>Sign Up</h1>
+    </div>
+  )
+}
+
+export default SignUp
