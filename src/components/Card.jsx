e1cec8e9b1a994cabad45437ff6e2feeb273922e
Sarthak-Srivastava-01 <sarthak.srivastava1398@gmail.com>
2024-09-08 09:45:12 +0530

    card changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,17 +8,17 @@ const Card = () => {
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6 px-8">
                     <div className="bg-white p-6 shadow rounded-lg">
                         <img src="https://img.freepik.com/premium-photo/colorful-abstract-painting-with-black-background_204916-535.jpg" alt="" />
-                        <h4 className="text-xl font-semibold mb-2">Feature 1</h4>
+                        <h4 className="text-xl font-semibold my-2">Feature 1</h4>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio eveniet consequatur quae assumenda laborum, dignissimos aut nobis ea officiis corporis?</p>
                     </div>
                     <div className="bg-white p-6 shadow rounded-lg">
                         <img src="https://img.freepik.com/premium-photo/colorful-abstract-painting-with-black-background_204916-535.jpg" alt="" />
-                        <h4 className="text-xl font-semibold mb-2">Feature 1</h4>
+                        <h4 className="text-xl font-semibold my-2">Feature 1</h4>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio eveniet consequatur quae assumenda laborum, dignissimos aut nobis ea officiis corporis?</p>
                     </div>
                     <div className="bg-white p-6 shadow rounded-lg">
                         <img src="https://img.freepik.com/premium-photo/colorful-abstract-painting-with-black-background_204916-535.jpg" alt="" />
-                        <h4 className="text-xl font-semibold mb-2">Feature 1</h4>
+                        <h4 className="text-xl font-semibold my-2">Feature 1</h4>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio eveniet consequatur quae assumenda laborum, dignissimos aut nobis ea officiis corporis?</p>
                     </div>
                 </div>
