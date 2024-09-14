bec6d9d214ea00a0b6ad5d03168e84ef52d2d22b
Sarthak-Srivastava-01 <sarthak.srivastava1398@gmail.com>
2024-09-14 09:41:17 +0530

    form validation

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,9 +1,28 @@
+'use client';
 import React from 'react'
 // import classes from './login.module.css'
 import Input from '@/components/Input';
 import Button from '@/components/Button';
+import { useFormik } from 'formik';
+import * as Yup from 'yup'
+
+const loginSchema = Yup.object().shape({
+  email : Yup.string().required('Email is required').email('Email is invalid'),
+  password : Yup.string().required('Password is required').min(5, 'Password must be at least 5 characters')
+})
 
 const Login = () => {
+    const loginForm =  useFormik({
+      initialValues : {
+        email : '',
+        password : ''
+      },
+      onSubmit : (values) => {
+        console.log(values);
+      },
+      validationSchema : loginSchema
+    })
+
   return (
     <div className='flex items-center justify-center'>
         
@@ -14,7 +33,7 @@ const Login = () => {
               <p className="mt-2 text-sm text-gray-600">
                 Don't have an account yet?
                 <a
-                  className="text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium"
+                  className="ml-2 text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium"
                   href="/signup"
                 >
                   Sign up here
@@ -22,41 +41,8 @@ const Login = () => {
               </p>
             </div>
             <div className="mt-5">
-              <button
-                type="button"
-                className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
-              >
-                <svg
-                  className="w-4 h-auto"
-                  width={46}
-                  height={47}
-                  viewBox="0 0 46 47"
-                  fill="none"
-                >
-                  <path
-                    d="M46 24.0287C46 22.09 45.8533 20.68 45.5013 19.2112H23.4694V27.9356H36.4069C36.1429 30.1094 34.7347 33.37 31.5957 35.5731L31.5663 35.8669L38.5191 41.2719L38.9885 41.3306C43.4477 37.2181 46 31.1669 46 24.0287Z"
-                    fill="#4285F4"
-                  />
-                  <path
-                    d="M23.4694 47C29.8061 47 35.1161 44.9144 39.0179 41.3012L31.625 35.5437C29.6301 36.9244 26.9898 37.8937 23.4987 37.8937C17.2793 37.8937 12.0281 33.7812 10.1505 28.1412L9.88649 28.1706L2.61097 33.7812L2.52296 34.0456C6.36608 41.7125 14.287 47 23.4694 47Z"
-                    fill="#34A853"
-                  />
-                  <path
-                    d="M10.1212 28.1413C9.62245 26.6725 9.32908 25.1156 9.32908 23.5C9.32908 21.8844 9.62245 20.3275 10.0918 18.8588V18.5356L2.75765 12.8369L2.52296 12.9544C0.909439 16.1269 0 19.7106 0 23.5C0 27.2894 0.909439 30.8731 2.49362 34.0456L10.1212 28.1413Z"
-                    fill="#FBBC05"
-                  />
-                  <path
-                    d="M23.4694 9.07688C27.8699 9.07688 30.8622 10.9863 32.5344 12.5725L39.1645 6.11C35.0867 2.32063 29.8061 0 23.4694 0C14.287 0 6.36607 5.2875 2.49362 12.9544L10.0918 18.8588C11.9987 13.1894 17.25 9.07688 23.4694 9.07688Z"
-                    fill="#EB4335"
-                  />
-                </svg>
-                Sign in with Google
-              </button>
-              <div className="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-1 before:border-t before:border-gray-200 before:me-6 after:flex-1 after:border-t after:border-gray-200 after:ms-6">
-                Or
-              </div>
               {/* Form */}
-              <form>
+              <form onSubmit={loginForm.handleSubmit}>
                 <div className="grid gap-y-4">
                   {/* Form Group */}
                   <div>
@@ -64,7 +50,14 @@ const Login = () => {
                       Email address
                     </label>
                     <div className="relative">
-                      <Input type='email' placeholder='john@doe.com'/>
+                      <input type="email" id='email' onChange={loginForm.handleChange} value={loginForm.values.email}
+                        className='w-full p-2 border rounded-lg my-2 bg-gray-100'
+                      />
+                      {
+                        (loginForm.touched.email && loginForm.errors.email) && (
+                          <p className='text-red-500 mb-5 text-sm'>{loginForm.errors.email}</p>
+                        )
+                      }
                       <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
                         <svg
                           className="size-5 text-red-500"
@@ -97,7 +90,14 @@ const Login = () => {
                       </a>
                     </div>
                     <div className="relative">
-                      <Input type='password' placeholder='**********'/>
+                      <input type="password" id='password' onChange={loginForm.handleChange} value={loginForm.values.password}
+                        className='w-full p-2 border rounded-lg my-2 bg-gray-100'
+                      />
+                      {
+                        (loginForm.touched.password && loginForm.errors.password) && (
+                          <p className='text-red-500 mb-5 text-sm'>{loginForm.errors.password}</p>
+                        )
+                      }
                       <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
                         <svg
                           className="size-5 text-red-500"
@@ -133,7 +133,9 @@ const Login = () => {
                     </div>
                   </div>
                   {/* End Checkbox */}
-                  <Button>Sign In</Button>
+                  <button
+                    type='submit'
+                    className='bg-black px-3 py-2 rounded text-white font-bold w-full'>Submit</button>
                 </div>
               </form>
               {/* End Form */}
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -3,6 +3,20 @@ import Button from '@/components/Button'
 import TextInput from '@/components/Input'
 import { useFormik } from 'formik'
 import React from 'react'
+import * as Yup from 'yup'
+
+const signupSchema = Yup.object().shape({
+  name : Yup.string().required('Name is Required').min(3, 'Name must be at least 3 characters'),
+  email : Yup.string().required('Email is required').email('Email is invalid'),
+  password : Yup.string().required('Password is required')
+    .min(5, 'Password must be at least 5 characters')
+    .matches(/[A-Z]/,'Password must contain an uppercase letter')
+    .matches(/[a-z]/,'Password must contain an lowercase letter')
+    .matches(/[0-9]/,'Password must contain a number')
+    .matches(/[\W]/,'Password must contain a special character'),
+  confirmPassword : Yup.string().required('Confirm Password is required')
+    .oneOf([Yup.ref('password'), null], 'Passwords must match'),
+});
 
 const Signup = () => {
 
@@ -10,12 +24,14 @@ const Signup = () => {
     initialValues : {
       name : '',
       email : '',
-      password : ''
+      password : '',
+      confirmPassword : ''
     },
     onSubmit: (values) => {
       console.log(values);
       // Send values to backend
-    }
+    },
+    validationSchema : signupSchema
   })
 
   return (
@@ -28,15 +44,41 @@ const Signup = () => {
           <input type="text" id='name' onChange={signupForm.handleChange} value={signupForm.values.name}
             className='w-full p-2 border rounded-lg my-2 bg-gray-100'
           />
+          {
+            (signupForm.touched.name && signupForm.errors.name) && (
+              <p className='text-red-500 mb-5 text-sm'>{signupForm.errors.name}</p>
+            )
+          }
 
           <label htmlFor="email">Email Address</label>
           <input type="email" id='email' onChange={signupForm.handleChange} value={signupForm.values.email}
             className='w-full p-2 border rounded-lg my-2 bg-gray-100'
           />
+          {
+            (signupForm.touched.email && signupForm.errors.email) && (
+              <p className='text-red-500 mb-5 text-sm'>{signupForm.errors.email}</p>
+            )
+          }
+
           <label htmlFor="password">Password</label>
           <input type="password" id='password' onChange={signupForm.handleChange} value={signupForm.values.password}
             className='w-full p-2 border rounded-lg my-2 bg-gray-100'
           />
+          {
+            (signupForm.touched.password && signupForm.errors.password) && (
+              <p className='text-red-500 mb-5 text-sm'>{signupForm.errors.password}</p>
+            )
+          }
+
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input type="password" id='confirmPassword' onChange={signupForm.handleChange} value={signupForm.values.confirmPassword}
+            className='w-full p-2 border rounded-lg my-2 bg-gray-100'
+          />
+          {
+            (signupForm.touched.confirmPassword && signupForm.errors.confirmPassword) && (
+              <p className='text-red-500 mb-5 text-sm'>{signupForm.errors.confirmPassword}</p>
+            )
+          }
 
           <button
             type='submit'
