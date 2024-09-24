089a01b93040d8b02da0846530def02c76eabfc6
Sarthak-Srivastava-01 <sarthak.srivastava1398@gmail.com>
2024-09-24 11:48:22 +0530

    display and delete data in db

---
+++ src/app/manageuser/page.jsx
@@ -0,0 +1,77 @@
+'use client';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import toast from 'react-hot-toast';
+
+const ManageUser = () => {
+
+    const [userList, setuserList] = useState([])
+
+
+    const fetchUsers = async () => {
+        const res = await axios.get('http://localhost:5000/user/getall')
+        console.log(res.status);
+        console.table(res.data);
+        setuserList(res.data)
+    }
+    
+    useEffect(() => {
+      fetchUsers();
+    }, [])
+
+    const deleteUser = (id) => {
+        axios.delete('http://localhost:5000/user/delete/' + id)
+        .then((result) => {
+            toast.success('User Deleted Successfully');
+            fetchUsers();
+        }).catch((err) => {
+            console.log(err);
+            toast.error('Something went wrong');
+        });
+    }
+    
+
+  return (
+    <div className='lg:max-w-[80%] mx-auto py-10'>
+        <div className='border rounded-xl shadow-lg p-8'>
+            <h1 className='text-center font-bold text-3xl mb-1'>Manage Users</h1>
+            <hr />
+            <table className='w-full'>
+                <thead className='bg-violet-800'>
+                    <tr className='text-white'>
+                        <th className='p-2 border border-white'>Sr. No.</th>
+                        <th className='p-2 border border-white'>Id</th>
+                        <th className='p-2 border border-white'>Name</th>
+                        <th className='p-2 border border-white'>Email</th>
+                        <th className='p-2 border border-white'>City</th>
+                        <th colSpan={2} className='p-2 border border-white'>Action</th>
+                    </tr>
+                </thead>
+                <tbody className='bg-violet-200'>
+                    {
+                        userList.map((user, index) => {
+                            return <tr key={user._id}>
+                                <td className='p-2 border border-violet-700'>{index+1}</td>
+                                <td className='p-2 border border-violet-700'>{user._id}</td>
+                                <td className='p-2 border border-violet-700'>{user.name}</td>
+                                <td className='p-2 border border-violet-700'>{user.email}</td>
+                                <td className='p-2 border border-violet-700'>{user.city}</td>
+                                <td className='p-2 border border-violet-700 text-center'>
+                                    <button
+                                     onClick={() => deleteUser(user._id)}
+                                     className='bg-red-500 text-white px-4 py-2 rounded-lg'>Delete</button>
+                                </td>
+                                <td className='p-2 border border-violet-700 text-center'>
+                                    <button className='bg-blue-500 text-white px-4 py-2 rounded-lg'>Update</button>
+                                </td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    </div>
+  )
+}
+
+export default ManageUser;
